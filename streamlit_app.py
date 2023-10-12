2730ff26662c518df5414eae41dbd147909e57ea
Jan Meuser <58228946+janmeuser@users.noreply.github.com>
2023-10-12 13:18:18 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,9 +13,9 @@ such as the number of rooms, the number of bedrooms, the population of the house
 The app preprocesses the input data by combining some of the features and adding new features, such as the distance to the nearest city.
 """)
 
-option = st.selectbox("Wähle eine Option", ["Option 1", "Option 2", "Option 3"])
-st.write("Ausgewählte Option:", option)
+# Lese den Datensatz aus einer CSV-Datei
+data = pd.read_csv('dein_datensatz.csv')
 
-df = pd.DataFrame(np.random.randn(50, 20), columns=("col %d" % i for i in range(20)))
-
-st.dataframe(df)  # Same as st.write(df)
+# Anzeige des Datensatzes als Tabelle
+st.write("Hier ist der Datensatz als Tabelle:")
+st.table(data
