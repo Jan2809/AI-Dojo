a7203d6037f08fe75c7823e3c55e74d01dd19fca
Jan Meuser <58228946+janmeuser@users.noreply.github.com>
2023-10-12 11:22:35 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from streamlit_extras.stoggle import stoggle
 
 st.title("🏡 California House Price Prediction")
 st.subheader("Check out the colab workbook 🔗.")
@@ -10,3 +11,8 @@ It loads a pre-trained linear regression model, which takes as input various fea
 such as the number of rooms, the number of bedrooms, the population of the house's neighborhood, and the distance to the nearest city. 
 The app preprocesses the input data by combining some of the features and adding new features, such as the distance to the nearest city.
 """)
+
+stoggle(
+    "Click me!",
+    """🥷 Surprise! Here's some additional content""",
+)
