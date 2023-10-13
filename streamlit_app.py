731ac07b16324a35f83e9d2c116c7fc367c5cba7
Jan Meuser <58228946+janmeuser@users.noreply.github.com>
2023-10-13 08:54:34 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,20 +9,42 @@ selected = option_menu(None, ["Home", "Data Set", "Classification", 'About'],
     menu_icon="cast", default_index=0, orientation="horizontal")
 selected
 
-st.title("🏡 California House Price Prediction")
-st.subheader("Check out the colab workbook 🔗.")
-st.markdown("""
-##### A web application for predicting California Housing Prices.
- 
-This app uses machine learning to predict the price of the house. 
-It loads a pre-trained linear regression model, which takes as input various features of the house, 
-such as the number of rooms, the number of bedrooms, the population of the house's neighborhood, and the distance to the nearest city. 
-The app preprocesses the input data by combining some of the features and adding new features, such as the distance to the nearest city.
-""")
+#Inhalt der Menü Punkte
+if selected == "Home":
+    st.title("🏡 California House Price Prediction")
+    st.subheader("Check out the colab workbook 🔗.")
+    st.markdown("""
+    ##### A web application for predicting California Housing Prices.
+    
+    This app uses machine learning to predict the price of the house. 
+    It loads a pre-trained linear regression model, which takes as input various features of the house, 
+    such as the number of rooms, the number of bedrooms, the population of the house's neighborhood, and the distance to the nearest city. 
+    The app preprocesses the input data by combining some of the features and adding new features, such as the distance to the nearest city.
+    """)
 
-# Lese den Datensatz aus einer CSV-Datei
-data = pd.read_csv('housing_new.csv')
+    
 
-# Anzeige des Datensatzes als Tabelle
-st.write("Hier ist der Datensatz als Tabelle:")
-st.table(data.head(21))
+
+    
+elif selected == "Data Set":
+       # Lese den Datensatz aus einer CSV-Datei
+    #data = pd.read_csv('housing_new.csv')
+    data = pd.read_csv('Basics/sample_data/california_housing_test.csv')
+
+    # Anzeige des Datensatzes als Tabelle
+    st.write("Hier ist der Datensatz als Tabelle:")
+    st.table(data.head(21))
+
+    
+elif selected == "Classification":
+    st.title("Einstellungen")
+    st.write("Passen Sie Ihre Einstellungen an.")
+elif selected == "About":
+
+
+    st.title("ABout California House Price Prediction")
+    st.subheader("Check out the colab workbook 🔗.")
+    
+
+
+
